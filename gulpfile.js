3bee32c834d3ccaf3427f312c51d22166da883ec
ministori <ministori@gmail.com>
2017-07-14 14:32:41 +0900

    UI Component

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,3 @@
-/**
- * Created by Administrator on 2017-07-03.
- */
-
 // 변수선언 = require('모듈이름');
 var gulp = require('gulp');
 var livereload = require('gulp-livereload');
@@ -10,24 +6,6 @@ var sass = require('gulp-sass');
 var watch = require('gulp-watch');
 var sourcemaps = require('gulp-sourcemaps');
 var concat = require('gulp-concat');
-
-/*
-
-gulp.task( task이름, 함수/익명함수 );
-
- */
-
-gulp.task('hello1', function () {
-  return console.log('Hello World1!');
-});
-
-gulp.task('hello2', function () {
-  return console.log('Hello World2!');
-});
-
-gulp.task('hello3', function () {
-  return console.log('Hello World3!');
-});
 
 // pipe()는 모듈의 기능을 실행해주는 함수
 
