9d94f6b03e8c281d9e26ac36fea6d308c7dadb16
ministori <ministori@gmail.com>
2017-07-17 15:29:15 +0900

    UI Component

--- js_src/image_sliding/_image_sliding_css.js
+++ js_src/image_sliding/_image_sliding_css.js
@@ -6,9 +6,9 @@ $(function(){
 
   // 선언부
   function init(){
-    $('.css-sliding-view-image').eq(0).addClass('current');
-    $('.css-sliding-view-image').eq(1).addClass('next');
-    $('.css-sliding-view-image').eq(2).addClass('prev');
+    $('.css-sliding-view-image').eq(0).addClass('center');
+    $('.css-sliding-view-image').eq(1).addClass('right');
+    $('.css-sliding-view-image').eq(2).addClass('left');
 
     //marginCtrlWrap();
     //paging();
@@ -22,22 +22,22 @@ $(function(){
       nextIndex = 0;
     }
 
-    $('.css-sliding-view-image').eq(currentIndex-1).removeClass('prev ani').addClass('next');
-    $('.css-sliding-view-image').eq(currentIndex).removeClass('current ani').addClass('prev ani');
-    $('.css-sliding-view-image').eq(nextIndex).removeClass('next').addClass('current ani');
+    $('.css-sliding-view-image').eq(currentIndex-1).removeClass('left ani').addClass('right');
+    $('.css-sliding-view-image').eq(currentIndex).removeClass('center ani').addClass('left ani');
+    $('.css-sliding-view-image').eq(nextIndex).removeClass('right').addClass('center ani');
 
     currentIndex = nextIndex;
     nextIndex++;
   }
 
   function moveRight(){
-    if(nextIndex >= $('.css-sliding-view-image').length){
-      nextIndex = 0;
+    if(nextIndex <= -1){
+      nextIndex = $('.css-sliding-view-image').length-1;
     }
 
-    $('.css-sliding-view-image').eq(currentIndex-1).removeClass('prev ani').addClass('next');
-    $('.css-sliding-view-image').eq(currentIndex).removeClass('current ani').addClass('prev ani');
-    $('.css-sliding-view-image').eq(nextIndex).removeClass('next').addClass('current ani');
+    $('.css-sliding-view-image').eq(currentIndex+1).removeClass('right ani').addClass('left');
+    $('.css-sliding-view-image').eq(currentIndex).removeClass('current ani').addClass('right ani');
+    $('.css-sliding-view-image').eq(nextIndex).removeClass('left').addClass('center ani');
 
     currentIndex = nextIndex;
     nextIndex--;
@@ -46,13 +46,34 @@ $(function(){
   // 실행부
   init();
 
-  var activeClick = function(){
-    nextIndex = currentIndex + 1;
-    moveLeft();
+  var activeClick = function(direction){
+
+    var dir = direction;
+
+    if( dir == 'left' ){
+      nextIndex = currentIndex + 1;
+      moveLeft();
+    } else {
+      nextIndex = currentIndex - 1;
+      moveRight();
+    }
+
     setTimeout(function(){
       // 재귀함수
-      $('.css-sliding-arrow.right').one('click', activeClick);
+
+      if( dir == 'left' ){
+        $('.css-sliding-arrow.right').one('click', function(){
+          activeClick(dir);
+        });
+      } else {
+        $('.css-sliding-arrow.left').one('click', function(){
+          activeClick(dir);
+        });
+      }
+      
+
     }, 1000);
+
   };
 
 
